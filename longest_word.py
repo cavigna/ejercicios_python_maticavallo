e888b805638d143462f867cc827f85663f80bc64
Ignacio Cavallo <40524472+cavigna@users.noreply.github.com>
2022-06-01 08:24:30 -0400

    Added Longest Word Challenge

---
+++ longest_word.py
@@ -0,0 +1,13 @@
+#Encontrar dentro de una string, aquella palabra con la mayor cantidad de letras.
+
+lorem = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mollis facilisis molestie.
+In sed elit ut mauris iaculis porttitor. Vestibulum ac justo lobortis, rhoncus eros sed, 
+feugiat ante. Nunc interdum, nunc at pellentesque facilisis, urna libero venenatis 
+purus, sed dignissim orci diam id urna. Aenean placerat leo eget felis efficitur.
+"""
+
+def longest_word(string: str)->str:
+    pass
+
+#Expected ==> La palabra más grande es: pellentesque con 12 letras
