ad6c3d8eb898abe4cf2be48a69cce9a9bc7dd01c
Ignacio Cavallo <40524472+cavigna@users.noreply.github.com>
2022-06-07 20:21:40 -0400

    Added repeated_elements

---
+++ repeated_elements.py
@@ -0,0 +1,11 @@
+#Encontrar los calores repetidos en una lista y mostrarlos en una nueva lista
+
+def find_repeated(listado:list)->list:
+    pass
+
+
+lista_numeros = [12, 23, 4, 6, 22,99,37, 52, 12, 65, 99, 33, 37]
+
+repetidos = find_repeated(lista_numeros)
+
+print(repetidos) #Expected ==> [12, 99, 37]
