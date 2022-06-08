d9e7f0c682d1ef844adb921daee83e72b21e1faa
Ignacio Cavallo <40524472+cavigna@users.noreply.github.com>
2022-06-07 22:06:41 -0400

    Added list_reversed.py

---
+++ list_reversed.py
@@ -0,0 +1,7 @@
+# Dada una lista devolver otro listado con los elementos en reversa.
+# No usar reverse()
+
+def list_revers(listado: list) -> list:
+    pass
+
+list_revers([1,9,12,42,5]) # Expected [5, 42, 12, 9, 1]
